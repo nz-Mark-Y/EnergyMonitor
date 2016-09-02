f43802a2ffb416e1e30290359608791a7e71083f
Mark Yep <ooksei@outlook.com>
2016-09-02 18:18:30 +1200

    firebase integrated

--- app/src/main/java/electeng209/energymonitor/MainActivity.java
+++ app/src/main/java/electeng209/energymonitor/MainActivity.java
@@ -2,12 +2,44 @@ package electeng209.energymonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView dataDisplay = (TextView)findViewById(R.id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                dataDisplay.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                dataDisplay.setText("Failed to read value." + error.toException());
+            }
+        });
     }
+
+
 }
