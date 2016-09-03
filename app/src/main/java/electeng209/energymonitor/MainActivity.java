6016b8cf845983208469b1cf8775b305a8d3eaea
Mark Yep <ooksei@outlook.com>
2016-09-03 14:22:35 +1200

    fully integrated with firebase

--- app/src/main/java/electeng209/energymonitor/MainActivity.java
+++ app/src/main/java/electeng209/energymonitor/MainActivity.java
@@ -2,6 +2,7 @@ package electeng209.energymonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -9,6 +10,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,8 +37,42 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                dataDisplay.setText(value);
+                Map<String, String> myMap = (Map)dataSnapshot.getValue();
+                if (!(myMap == null)) {
+                    String[] dataArray = myMap.values().toArray(new String[0]);
+                    ArrayList<MyData> dataArrayList = new ArrayList<>();
+                    int number;
+                    int value;
+                    /*
+                    try {
+                        JSONObject obj = new JSONObject(dataArray[dataArray.length-1]);
+                        number = obj.getInt("number");
+                        value = obj.getInt("value");
+                    }  catch(JSONException e) {
+                        number = 0;
+                        value = 0;
+                        dataDisplay.setText(e.getMessage());
+                    }
+                    dataDisplay.setText("Number is:" + number + "Value is: " + value);
+                    */
+
+                    for (int i=0;i<dataArray.length;i++) {
+                        try {
+                            JSONObject obj = new JSONObject(dataArray[i]);
+                            number = obj.getInt("number");
+                            value = obj.getInt("value");
+                        }  catch(JSONException e) {
+                            number = 0;
+                            value = 0;
+                            dataDisplay.setText(e.getMessage());
+                        }
+                        MyData myData = new MyData(number, value);
+                        Log.d("MyData: " + String.valueOf(myData.number), String.valueOf(myData.value));
+                        dataArrayList.add(myData);
+                    }
+                    dataArrayList = sortArray(dataArrayList);
+                    dataDisplay.setText("Number is:" + dataArrayList.get(dataArrayList.size()-1).number + "Value is: " + dataArrayList.get(dataArrayList.size()-1).value);
+                }
             }
 
             @Override
@@ -40,5 +81,17 @@ public class MainActivity extends AppCompatActivity {
                 dataDisplay.setText("Error: " + error.toException());
             }
         });
+    }
+
+    public ArrayList<MyData> sortArray(ArrayList<MyData> inputList) {
+        for(int i=1;i<inputList.size();i++) {
+            MyData temp;
+            if (inputList.get(i-1).number > inputList.get(i).number) {
+                temp = inputList.get(i-1);
+                inputList.set(i-1,inputList.get(i));
+                inputList.set(i, temp);
+            }
+        }
+        return inputList;
     }
 }
---
+++ app/src/main/java/electeng209/energymonitor/MyData.java
@@ -0,0 +1,14 @@
+package electeng209.energymonitor;
+
+/**
+ * Created by Mark Yep on 3/09/2016.
+ */
+public class MyData {
+    public int number;
+    public int value;
+
+    public MyData(int number, int value) {
+        this.number = number;
+        this.value = value;
+    }
+}
